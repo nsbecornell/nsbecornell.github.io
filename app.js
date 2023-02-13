7a8cdacd9f7e4ab190e03109b4ccb49c464fba29
ioi3 <ioi3@cornell.edu>
2023-02-13 11:55:53 -0500

    Added the actual calendar

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
   var calendarEl = document.getElementById('calendar');
 
-  fetch('https://www.googleapis.com/calendar/v3/calendars/cornellnsbe@gmail.com/events?key=AIzaSyBHd2s6Rw3i2FhDUNtfjBWWTi1e7M3femc')
+  fetch('https://www.googleapis.com/calendar/v3/calendars/fvh1d17oek2s9mg8mq0a6vepr0@group.calendar.google.com/events?key=AIzaSyBHd2s6Rw3i2FhDUNtfjBWWTi1e7M3femc')
     .then(response => response.json())
     .then(data => {
-
-      var events = data.items.map(event => ({
+      // TODO: Descriptions, links, etc.
+      var events = data.items.filter(event=>event.status!="cancelled").map(event => ({
         title: event.summary,
         start: event.start.dateTime || event.start.date,
         end: event.end.dateTime || event.end.date,
@@ -19,7 +19,7 @@ document.addEventListener('DOMContentLoaded', function () {
           center: 'title',
           right: 'next'
         },
-        displayEventTime: true,
+        displayEventTime: false,
         events: events
       });
 
