650f8817202b5f9a0c7b873b91deeac330e21bf0
Chimdi <chimdije@gmail.com>
2024-08-23 15:14:11 -0400

    Replace dummy text for mission and vision statement on home page with valid text

--- nsbe-website/src/app/page.tsx
+++ nsbe-website/src/app/page.tsx
@@ -37,33 +37,39 @@ export default function Home() {
           <Heading position="absolute" sx={headingStyling}>Black Engineers at Cornell University</Heading>
         </Center>
       </Flex>
+
+
       <Box as="section" className={styles.introSection}>
         <Heading sx={headingStyling}>Who We Are</Heading>
         <Text sx={textStyle}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse condimentum eget lacus gravida lacinia. Suspendisse viverra tellus a justo finibus, eu blandit purus egestas. Aliquam porttitor volutpat tellus eget scelerisque. In lobortis quis nisi vel malesuada. Quisque lectus est, suscipit ut sapien eu, pharetra accumsan mauris. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In sagittis justo pulvinar urna posuere posuere. Integer dui magna, feugiat eu arcu et, placerat feugiat turpis. Praesent lacinia enim vel pretium cursus. Integer sodales turpis nibh, vehicula elementum magna iaculis id. Phasellus sapien libero, imperdiet sed faucibus vitae, feugiat vel nulla. Integer eleifend orci sit amet enim dignissim, vitae pharetra neque cursus.</Text>
         <Text sx={textStyle}>Nullam commodo sapien quis malesuada posuere. Nunc laoreet ipsum risus, id sagittis tortor blandit nec. Vivamus sollicitudin nisl sed quam lacinia molestie. Etiam mauris arcu, sodales dapibus maximus quis, bibendum at dui. Phasellus vel ante eu lacus ultricies pellentesque sed non sapien. Maecenas placerat ut tortor vitae laoreet. Praesent sagittis orci turpis, et pulvinar ipsum auctor a. Fusce id consectetur turpis, a tempus nulla. Cras auctor tincidunt velit, lobortis dapibus tellus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur molestie iaculis orci rhoncus imperdiet. Vestibulum ac nunc mattis, rutrum odio sed, suscipit arcu.</Text>
       </Box>
+
+
       <Box as="section" className={styles.valuesSection}>
         <Heading sx={headingStyling}>What We Stand For</Heading>
         <Flex direction="column" align="center">
           <HStack spacing={150}>
             <TextContainer
               title="Mission Statement"
-              message="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis rem facere esse nam eum hic corrupti asperiores dolorum, quam voluptatem, sunt commodi voluptatum nesciunt ipsum! Dolorum saepe quibusdam ipsa a!"
+              message="The NSBE mission statement is 'to increase the number of culturally responsible black engineers who excel academically, succeed professionally and positively impact the community.'"
             />
             <TextContainer
               title="Vision Statement"
-              message="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis rem facere esse nam eum hic corrupti asperiores dolorum, quam voluptatem, sunt commodi voluptatum nesciunt ipsum! Dolorum saepe quibusdam ipsa a!"
+              message="We envision a world in which engineering is a mainstream word in homes and communities of color, and all Black students can envision themselves as engineers. In this world, Black people exceed parity in entering engineering fields, earning degrees, and succeeding professionally."
             />
           </HStack>
         </Flex>
       </Box>
+
+
       <Box as="section">
         <Heading sx={headingStyling}>Initiatives</Heading>
         <Flex justify="center" gap="10%">
             <ImageContainer src={AcademicExcellence.src} fallbackSrc="" alt=""/>
             <Flex direction='column' wrap='wrap' align='center' justify="center" >
               <Heading sx={headingStyling} marginBottom={0} fontSize={25}>Academic Excellence</Heading>
-              <Text sx={textStyle} maxWidth={600} fontSize={20} fontWeight="bold">Our tutorial programs, or 'Study Jams', are held regularly throughout the school year, offering free tutoring and review sessions for general freshman and sophomore engineering courses and hosting post-exam celebrations. NSBE-CU facilitates academic excellence by rewarding deserving members with scholarships and providing mentorship and assistance to struggling students to help them reach their full academic potential.
+              <Text sx={textStyle} maxWidth={600} fontSize={20} fontWeight="bold">Our tutorial programs, or &apos;Study Jams&apos;, are held regularly throughout the school year, offering free tutoring and review sessions for general freshman and sophomore engineering courses and hosting post-exam celebrations. NSBE-CU facilitates academic excellence by rewarding deserving members with scholarships and providing mentorship and assistance to struggling students to help them reach their full academic potential.
               </Text>
             </Flex>
         </Flex>
