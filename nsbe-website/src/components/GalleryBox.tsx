7dc6f6b613ac45d1d81f910737b489e2c322cd36
Chimdi <chimdije@gmail.com>
2024-09-07 15:06:04 -0400

    Implement hover effect for gallery boxes, increase spacing between member image and text

--- nsbe-website/src/components/GalleryBox.tsx
+++ nsbe-website/src/components/GalleryBox.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import styles from "@/app/page.module.css";
 
 // Components
@@ -23,20 +25,28 @@ const GalleryBox = (props: {key: number, name: string, position: string, image: 
     my: 10,
     borderRadius: 25,
     h: 350,
-    w: 300
+    w: 300,
+    _hover: {
+      bg: "#3A3A3A"
+    }
   }
+
+  function handleClick() {
+    console.log("Clicked");
+  } 
+
 
   return (
     // Convert to GridRow component later, needs ChakraProvider
-    <Flex direction='column' sx={boxStyling}>
+    <Flex direction='column' sx={boxStyling} onClick={handleClick}>
       <Center h='inherit'>
         <VStack>
-        <Image borderRadius={12} width={250} h={250} objectFit="fill" src={props.image} alt=""/>
-        <VStack>
-          <Text sx={textStyling} fontSize={12} as="span" className={styles.positionTitle}>{props.position}</Text>
-          <Text sx={textStyling} fontSize={15} as="span" className={styles.memberName}>{props.name}</Text>
+          <Image borderRadius={12} width={250} h={250} objectFit="fill" src={props.image} alt=""/>
+          <VStack marginTop="3%">
+            <Text sx={textStyling} fontSize={12} as="span" className={styles.positionTitle}>{props.position}</Text>
+            <Text sx={textStyling} fontSize={15} as="span" className={styles.memberName}>{props.name}</Text>
+          </VStack>
         </VStack>
-      </VStack>
       </Center>
       
     </Flex>
