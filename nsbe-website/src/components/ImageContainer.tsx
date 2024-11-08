1b975675b58a4c298dbbdfff4bf64cdac007484b
Chimdi <chimdije@gmail.com>
2024-11-08 13:56:47 -0500

    Implement basic sponsor containers

--- nsbe-website/src/components/ImageContainer.tsx
+++ nsbe-website/src/components/ImageContainer.tsx
@@ -1,9 +1,9 @@
 // Chakra UI Components
 import { Flex, Image, Center } from '@chakra-ui/react'
 
-function ImageContainer(props: {src: string, alt: string, containerWidth: number, containerHeight: number, imageWidth: number, imageHeight: number}) {
+function ImageContainer(props: {src: string, alt: string, containerWidth: number, containerHeight: number, imageWidth: number, imageHeight: number, color?: string}) {
   const boxStyling = {
-    bg: "#2C2C2C",
+    bg:  props.color ? props.color : "#2C2C2C",
     w: props.containerWidth,
     h: props.containerHeight,
     borderRadius: 25,
--- nsbe-website/src/components/SideContainer.tsx
+++ nsbe-website/src/components/SideContainer.tsx
@@ -1,12 +1,12 @@
-import { Box, Flex, Text, Heading } from '@chakra-ui/react'
+import { Box, Flex, Text, Heading, Center, VStack} from '@chakra-ui/react'
 
 import ImageContainer from '@/components/ImageContainer'
 
 function SideContainer() {
   const boxStyling = {
-    bg: "gray",
-    w: "40%",
-    minWidth: "40%",
+    bg: "#2C2C2C",
+    w: 400,
+    borderRadius: 15
 
   }
 
@@ -21,11 +21,52 @@ function SideContainer() {
   }
 
   return (
-    <Box sx={boxStyling}>
-      <Heading sx={headingStyling}>
-        Corporate Sponsorships
-      </Heading>
-      <Box>
+    <Box sx={boxStyling} paddingLeft="1%" paddingRight='1%'>
+      <VStack spacing={0}>
+        <Heading sx={headingStyling}>
+          Corporate Sponsorships
+        </Heading>
+        <Text sx={textStyling} my={0}>
+          In order to realize our initiatives, we&apos;ll need your support! Your sponsorship will help fund professional development opportunities, community outreach initiatives, and critical resources that enable our members to excel academically and professionally.
+        </Text>
+        <Text fontWeight='bold' sx={textStyling} background='red'>
+          Silver
+        </Text>
+        <Text fontWeight='bold' sx={textStyling}>
+          Bronze
+        </Text>
+        <Text fontWeight='bold' sx={textStyling}>
+          Diamond
+        </Text>
+        <Box>
+          <Heading sx={headingStyling} textAlign='left' fontSize={22}>
+            How You Benefit
+          </Heading>
+          <Flex direction="column">
+          <Flex>
+           <ImageContainer src="" alt="" color='red' containerWidth={25} containerHeight={25} imageWidth={10} imageHeight={10}/>
+           <Text>
+              Diverse, Talented Candidates
+           </Text>
+          </Flex>
+          <Flex>
+           <ImageContainer src="" alt="" color='red' containerWidth={25} containerHeight={25} imageWidth={10} imageHeight={10}/>
+           <Text>
+              Contribute to a Cause
+           </Text>
+          </Flex>
+          <Flex>
+           <ImageContainer src="" alt="" color='red' containerWidth={25} containerHeight={25} imageWidth={10} imageHeight={10}/>
+           <Text>
+              Brand Visibility
+           </Text>
+          </Flex>
+        </Flex>
+        </Box>
+      </VStack>
+
+      
+      {/* <Box>
         <Text sx={textStyling}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse condimentum eget lacus gravida lacinia. Suspendisse viverra tellus a justo finibus, eu blandit purus egestas. Aliquam porttitor volutpat tellus eget scelerisque.
         </Text>
@@ -58,16 +99,13 @@ function SideContainer() {
            </Text>
           </Flex>
           <Flex>
-           <ImageContainer src="" alt="" containerWidth={25} containerHeight={25} imageWidth={10} imageHeight={10}/>
+           <ImageContainer src="" alt="" color='red' containerWidth={25} containerHeight={25} imageWidth={10} imageHeight={10}/>
            <Text>
               Brand Visibility
            </Text>
           </Flex>
-
         </Flex>
-      </Box>
-      
-
+      </Box> */}
     </Box>
   )
 }
