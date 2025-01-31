1bcd1e6313f288b2e4ab1c6f5afced74b6cbb674
Chimdi <chimdije@gmail.com>
2025-01-31 16:49:29 -0500

    Implement functionalities buttons on page

--- nsbe-website/src/app/getting-involved/students/page.tsx
+++ nsbe-website/src/app/getting-involved/students/page.tsx
@@ -1,7 +1,7 @@
 import styles from "@/app/page.module.css";
 
 // Chakra UI Imports
-import { Button, Flex, Heading, VStack, Spacer, Center} from '@chakra-ui/react'
+import { Link, Button, Flex, Heading, VStack, Spacer, Center} from '@chakra-ui/react'
 
 // Custom Imports
 import DescriptionSection from "@/components/DescriptionSection";
@@ -30,26 +30,34 @@ const Students = () => {
       <Flex direction='row'>
         <Flex direction='column' marginTop="5%">
           <Heading sx={headingStyle} textAlign='center'>Stay Connected!</Heading>
-          <Button sx={buttonStyle}>
-            <Heading sx={buttonTextStyle}>
-              Join Our List-Serv
-            </Heading>
-          </Button>
-          <Button sx={buttonStyle}>
-            <Heading sx={buttonTextStyle}>
-              Join Our Slack
-            </Heading>
-          </Button>
-          <Button sx={buttonStyle}>
-            <Heading sx={buttonTextStyle}>
-              Follow Our Instagram
-            </Heading>
-          </Button>
-          <Button sx={buttonStyle}>
-            <Heading sx={buttonTextStyle}>
-              Subscribe to Our Calendar
-            </Heading>
-          </Button>
+          <Link href="https://docs.google.com/forms/d/e/1FAIpQLSd70Z7zxJp1njIi8mc3Jnehvd5aU-TEsiVqSRl6Po84_SA9Hg/viewform" target="_blank" rel="noopener noreferrer">
+            <Button sx={buttonStyle}>
+              <Heading sx={buttonTextStyle}>
+                Join Our List-Serv
+              </Heading>
+            </Button>
+          </Link>
+          <Link href="https://join.slack.com/t/cornellnsbe/shared_invite/zt-1t417svmu-ZGqAghed07DBj0M9uiMeSA" target="_blank" rel="noopener noreferrer">
+            <Button sx={buttonStyle}>
+              <Heading sx={buttonTextStyle}>
+                Join Our Slack
+              </Heading>
+            </Button>
+          </Link>
+          <Link href="https://www.instagram.com/nsbe_cu/?hl=en" target="_blank" rel="noopener noreferrer">
+            <Button sx={buttonStyle}>
+              <Heading sx={buttonTextStyle}>
+                Follow Our Instagram
+              </Heading>
+            </Button>
+          </Link>
+          <Link href="https://forms.gle/TWNrDaZJPb6AidoL8" target="_blank" rel="noopener noreferrer">
+            <Button sx={buttonStyle}>
+              <Heading sx={buttonTextStyle}>
+                Subscribe to Our Calendar
+              </Heading>
+            </Button>
+          </Link>
         </Flex>
         <Spacer />
         <VStack marginRight="6%">
@@ -63,16 +71,20 @@ const Students = () => {
       <Center>
         <Flex justifyContent='center' direction="column">
           <Heading sx={headingStyle}>Executive Board Applications</Heading>
-          <Button sx={buttonStyle} w='fit-content'>
-            <Heading sx={buttonTextStyle} mx="2em">
-              General Executive Board Application
-            </Heading>
-          </Button>
-          <Button sx={buttonStyle} w='fit-content'>
-            <Heading sx={buttonTextStyle} mx="2em">
-              First-Year Representative Application
-            </Heading>
-          </Button>
+          <Link>
+            <Button sx={buttonStyle} w='fit-content'>
+              <Heading sx={buttonTextStyle} mx="2em">
+                General Executive Board Application
+              </Heading>
+            </Button>
+          </Link>
+          <Link>
+            <Button sx={buttonStyle} w='fit-content'>
+              <Heading sx={buttonTextStyle} mx="2em">
+                First-Year Representative Application
+              </Heading>
+            </Button>
+          </Link>
         </Flex>
       </Center>
       
