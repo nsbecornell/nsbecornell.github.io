b471d8e8499983be43845303d0acbfa422e24a14
Chimdi <chimdije@gmail.com>
2025-02-01 00:43:29 -0500

    Add sponsor logos and new tier section to corporate page

--- nsbe-website/src/app/getting-involved/corporate/page.tsx
+++ nsbe-website/src/app/getting-involved/corporate/page.tsx
@@ -7,25 +7,96 @@ import { Heading, Flex, Spacer, VStack } from '@chakra-ui/react';
 import SideContainer from '@/components/SideContainer'
 import SponsorContainer from '@/components/SponsorContainer'
 
+// Sponsor Logos
+import Corning from '@/images/sponsors/diamond-tier/corning.png'
+import JaneStreet from '@/images/sponsors/gold-tier/jane-street.png'
+import Sensata from '@/images/sponsors/gold-tier/sensata.png'
+import Pepsi from '@/images/sponsors/gold-tier/pepsi.png'
+import Accenture from '@/images/sponsors/gold-tier/accenture.png'
+import CapitalOne from '@/images/sponsors/gold-tier/capital-one.png'
+import Arcadis from '@/images/sponsors/gold-tier/arcadis logo.png'
+import ClarkConstruction from '@/images/sponsors/gold-tier/clark.png'
+
 const Corporate = () => {
   const headingStyle = {
     color: 'white',
     fontSize: 30
   }
 
+  type Sponsor = {
+    id: number,
+    name: string,
+    image: string,
+    link: string
+  }
+
+
+  const diamondSponsors: Sponsor[] = [
+    {
+      id: 1,
+      name: "Corning",
+      image: Corning.src,
+      link: "https://www.corning.com/worldwide/en.html"
+    }
+  ]
+
+
+  const goldSponsors : Sponsor[] = [
+    {
+      id: 1,
+      name: "Jane Street",
+      image: JaneStreet.src,
+      link: "https://www.janestreet.com/"
+    },
+    {
+      id: 2,
+      name: "Sensata",
+      image: Sensata.src,
+      link: "https://www.sensata.com/"
+    },
+    {
+      id: 3,
+      name: "Pepsi",
+      image: Pepsi.src,
+      link: "https://www.pepsico.com/"
+    },
+    {
+      id: 4,
+      name: "Accenture",
+      image: Accenture.src,
+      link: "https://www.accenture.com/us-en"
+    },
+    {
+      id: 5,
+      name: "Capital One",
+      image: CapitalOne.src,
+      link: "https://www.capitalone.com/"
+    },
+    {
+      id: 6,
+      name: "Arcadis",
+      image: Arcadis.src,
+      link: "https://www.arcadis.com/"
+    },
+    {
+      id: 7,
+      name: "Clark Construction",
+      image: ClarkConstruction.src,
+      link: "https://www.clarkconstruction.com/"
+    }
+  ]
+
   return (
     <main className={styles.main}>
-      <Flex direction="row">
+      <Flex direction="row" marginTop="1%">
         <SideContainer />
         <Spacer/>
         <VStack>
           <Heading sx={headingStyle}>
             Our Sponsors
           </Heading>
-          <SponsorContainer />
-          <SponsorContainer />
-          <SponsorContainer />
-          
+          <SponsorContainer tier="diamond" sponsorList={diamondSponsors} />
+          <SponsorContainer tier="gold" sponsorList={goldSponsors} />          
         </VStack>
       </Flex>
       
