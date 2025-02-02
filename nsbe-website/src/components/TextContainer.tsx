59f4dc29f20c3c8ef5d4c339c77a97615dd89679
Chimdi <chimdije@gmail.com>
2025-02-02 01:11:09 -0500

    Minor restyling, make images dynamic in size

--- nsbe-website/src/components/TextContainer.tsx
+++ nsbe-website/src/components/TextContainer.tsx
@@ -1,29 +1,27 @@
-import {Flex, VStack, Box, Center, Text, Heading, border } from '@chakra-ui/react'
+import {Flex, HStack, Box, Center, Text, Heading, border } from '@chakra-ui/react'
 
 const TextContainer = (props: {title: string, message: string}) => {
   const boxStyling = {
     bg: "#2C2C2C",
     borderRadius: 23,
-    w: 450
+    w: "50%",
+    _hover: {
+      transform: "scale(1.05)",
+    },
+    transition: "transform 0.3s ease-in-out, background-color 0.3s ease-in-out",
   }
 
   const textStyle = {
     color: "white",
-    maxWidth: 375,
+    maxWidth: "90%",
     wordWrap: "break-word"
   }
 
   return (
-    <Flex sx={boxStyling} direction='column' wrap='wrap' align='center' justify="center">
-      <Heading sx={textStyle} marginBottom={0} fontSize={20}>{props.title}</Heading>
-      <Text sx={textStyle} fontSize={17}>{props.message}</Text>
-    </Flex>
-    // <Flex direction="column"  sx={boxStyling}>
-    //   <Center>
-    //     <Heading>{props.title}</Heading>
-    //     <Text>{props.message}</Text>
-    //   </Center>
-    // </Flex>
+    <HStack sx={boxStyling} direction='column' wrap='wrap' align='center' justify="center" w="fit-content" gap={5} padding={5}>
+      <Heading sx={textStyle} marginBottom={0} fontSize={25}>{props.title}</Heading>
+      <Text sx={textStyle} fontSize={20}>{props.message}</Text>
+    </HStack>
   );
 }
 
