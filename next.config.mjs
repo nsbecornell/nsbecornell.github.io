69bd2356a5d6d32f876866bb7e1b1eaf24a64d3a
Chimdi <chimdije@gmail.com>
2025-02-02 12:24:09 -0500

    Bug fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: process.env.NODE_ENV === 'production' ? '/nsbecornell.github.io' : '', // Dynamically set basePath
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/nsbecornell.github.io/' : '', // Dynamically set assetPrefix
   images: {
     unoptimized: true,
   },
