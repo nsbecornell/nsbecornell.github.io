6b843b212cb47ba153eb00706affabe8cbadf436
Chimdi <chimdije@gmail.com>
2025-02-02 15:41:33 -0500

    Bug fixes

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,5 +1,8 @@
+'use client';
+
 import { Image, Flex, Link, Spacer, HStack} from '@chakra-ui/react';
 import NSBECULogo from '@/images/nsbe-cu logo.png'
+import {useState, useEffect} from 'react';
 
 const NavigationBar = () => {
 
@@ -17,23 +20,33 @@ const NavigationBar = () => {
     minWidth: "fit-content"
   }
 
+  const [basePath, setBasePath] = useState('');
+
+  useEffect(() => {
+    import('../../next.config.mjs')
+      .then((config) => {
+        setBasePath(config.default.basePath || '');
+      })
+      .catch((err) => console.error("Error loading config:", err));
+  }, []);
+
   return (
       <Flex as="nav" gap={10} my={10} mx="3.25%">
         {/* Consider fallback image*/}
-        <Link href='/'>
+        <Link href={`${basePath}/`}>
           <Image w="15%" minW='100px' src={NSBECULogo.src} alt="Logo of Cornell University National Society of Black Engineers"/>
         </Link>
         <Spacer />
 
         <HStack spacing={14}>
-          <Link sx={linkStyles} href='/about-us'>About Us</Link>
-          <Link sx={linkStyles} href='/leadership'>Leadership</Link>
+          <Link sx={linkStyles} href={`${basePath}/about-us`}>About Us</Link>
+          <Link sx={linkStyles} href={`${basePath}/leadership`}>Leadership</Link>
           {/* <Link sx={linkStyles} href=''>Events</Link> */}
           {/* <Link sx={linkStyles} href=''>Points</Link> */}
           {/* <Link sx={linkStyles} href=''>Gallery</Link> */}
-          <Link sx={linkStyles} href='/getting-involved/alumni'>Alumni</Link>
-          <Link sx={linkStyles} href='/getting-involved/students'>Students</Link>
-          <Link sx={linkStyles} href='/getting-involved/corporate'>Corporate</Link>
+          <Link sx={linkStyles} href={`${basePath}/getting-involved/alumni`}>Alumni</Link>
+          <Link sx={linkStyles} href={`${basePath}/getting-involved/students`}>Students</Link>
+          <Link sx={linkStyles} href={`${basePath}/getting-involved/corporate`}>Corporate</Link>
         </HStack>
       </Flex>
   );
