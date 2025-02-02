76ee8643bb49db21348ab6164e8544da9ddff8a7
Chimdi <chimdije@gmail.com>
2025-02-02 04:10:29 -0500

    Configurations for deployment

--- nsbe-website/next.config.mjs
+++ nsbe-website/next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: "export",
+    basePath: "/nsbe-website", // Ensure it matches your repository name
+    assetPrefix: "/nsbe-website/",
+    images: {
+      unoptimized: true, // Required for "output: export"
+    },
+  };
+  
+  export default nextConfig;
