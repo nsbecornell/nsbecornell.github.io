8ff938fa7505469c8719b93f9ff48a6128bc2e3e
Chimdi <chimdije@gmail.com>
2025-02-02 02:09:01 -0500

    Minor restyling of components

--- nsbe-website/src/app/getting-involved/alumni/page.tsx
+++ nsbe-website/src/app/getting-involved/alumni/page.tsx
@@ -1,19 +1,23 @@
 import styles from "@/app/page.module.css";
 
 // Chakra UI Imports
-import { Link, Box, Button, Flex, Heading, VStack, Text, Spacer, Center, textDecoration } from '@chakra-ui/react'
-import { Butterfly_Kids } from "next/font/google";
-import { BorderWidth } from "react-bootstrap-icons";
+import { Link, Box, Button, Flex, Heading, VStack, Text, Spacer, Center, textDecoration } from '@chakra-ui/react';
 
-const Students = () => {
-  const headingStyle = {
+const Alumni = () => {
+  const headingStyling = {
+    fontWeight: "bold",
     color: "white",
-    fontSize: 30
-  }
+    fontSize: "30",
+    my: 6,
+    textAlign: 'center'
+  };
 
   const textStyle = {
-    color: "white"
-  }
+    color: "white",
+    fontSize: "20",
+    textIndent: "3%",
+    lineHeight: 1.8
+  };
 
   const boxHeading = {
     color: "white",
@@ -24,11 +28,18 @@ const Students = () => {
 
   const boxStyle = {
     bg: "#2C2C2C",
-    mx: 12,
-    my: 10,
+    mx: 5,
+    my: 5,
     borderRadius: 10,
-    h: 175,
-    w: 175
+    minH: 175,
+    w: "20%",
+    _hover: {
+      bg: "#3A3A3A",
+      transform: "scale(1.05)",
+    },
+    transition: "transform 0.3s ease-in-out, background-color 0.3s ease-in-out",
+    justifyContent: 'center',
+    flexDir: 'column'
   }
 
   const boxTextStyle = {
@@ -41,28 +52,39 @@ const Students = () => {
     borderRadius: 15,
     bg: "#2C2C2C",
     borderWidth: 0,
-    marginBottom: 15
+    marginBottom: 15,
+    _hover: {
+      bg: "#3A3A3A",
+      transform: "scale(1.05)",
+    },
+    transition: "transform 0.3s ease-in-out, background-color 0.3s ease-in-out",
   }
 
   const donateButtonStyle = {
     borderRadius: 15,
     bg: "#2C2C2C",
     borderWidth: 0,
-    w: "40%",
-    minWidth: "35%"
+    padding: 8,
+    mx: 30,
+    my: 8,
+    _hover: {
+      bg: "#3A3A3A",
+      transform: "scale(1.05)",
+    },
+    transition: "transform 0.3s ease-in-out, background-color 0.3s ease-in-out",
   }
 
   const buttonTextStyle = {
     color: "white",
     fontSize: 20,
-    textDecoration: ""
+    mx: 20
   }
 
   return (
     <main className={styles.main}>
-      <Flex flexDir="row" flexWrap='wrap'>
-        <Flex direction="column" marginTop="5%">
-          <Heading sx={headingStyle} textAlign='center'>Stay Connected!</Heading>
+      <Flex flexDir="row" flexWrap='nowrap'>
+        <Flex direction="column" margin="5%">
+          <Heading sx={headingStyling} textAlign='center'>Stay Connected!</Heading>
           <Link href="http://eepurl.com/ikM-wr" target="_blank" rel="noopener noreferrer">
             <Button
               sx={buttonStyle}
@@ -80,74 +102,73 @@ const Students = () => {
             </Button>
           </Link>
         </Flex>
-        <Spacer />
-        <VStack marginRight="6%">
+        <VStack marginRight="6%" flexGrow={1}>
           <Box as="section">
             <Flex direction="column" justify="center">
-              <Heading sx={headingStyle} textAlign='center'>Donations Help Us Realize Our Initiatives</Heading>
-              <Text maxW={750} sx={textStyle}>
+              <Heading sx={headingStyling} textAlign='center'>Donations Help Us Realize Our Initiatives</Heading>
+              <Text sx={textStyle}>
                 Your support can make a lasting impact on the future of aspiring Black engineers at Cornell University. By donating to our NSBE chapter, you contribute to the mission of NSBE: to increase the number of culturally responsible Black engineers who excel academically, succeed professionally, and positively impact the community. Your generosity will directly support our initiatives in academic excellence, community service, and professional development, as well as help to send current members to NSBE conferences, provide essential resources for academic success, and expand our community service efforts. Together, we can empower the next generation of leaders and innovators. Join us in advancing this mission—your donation today will help our members achieve their goals and create a brighter future for all.
               </Text>
             </Flex>
           </Box>
-          <Flex direction="row" wrap="wrap" maxW={800} justify='center'>
-            <Box sx={boxStyle}>
+          <Flex direction="row" wrap="wrap" justify='center'>
+            <Flex sx={boxStyle}>
               <Heading sx={boxHeading}>
                 $15
               </Heading>
               <Text sx={boxTextStyle}>
                 Covers the cost for National NSBE membership for one student.
               </Text>
-            </Box>
-            <Box sx={boxStyle}>
-              <Heading sx={boxHeading}>$20</Heading>
+            </Flex>
+            <Flex sx={boxStyle}>
+              <Heading sx={{...headingStyling, my: 3}}>$20</Heading>
               <Text sx={boxTextStyle}>
               Covers chapter dinner for one member at Regional or National competition.
               </Text>
-            </Box>
-            <Box sx={boxStyle}>
+            </Flex>
+            <Flex sx={boxStyle}>
               <Heading sx={boxHeading}>
                 $45
               </Heading>
               <Text sx={boxTextStyle}>
                 Covers the cost for transportation to FRC for one student.
               </Text>
-            </Box>
-            <Box sx={boxStyle}>
+            </Flex>
+            <Flex sx={boxStyle}>
               <Heading sx={boxHeading}>
                 $90
               </Heading>
               <Text sx={boxTextStyle}>
                 Covers all of a student&apos;s registration for the Fall Regional Conference.
               </Text>
-            </Box>
-            <Box sx={boxStyle}>
+            </Flex>
+            <Flex sx={boxStyle}>
               <Heading sx={boxHeading}>
                 $150
               </Heading>
               <Text sx={boxTextStyle}>
                 Covers all of a student&apos;s registration for the Fall Regional Conference.
               </Text>
-            </Box>
-            <Box sx={boxStyle}>
+            </Flex>
+            <Flex sx={boxStyle}>
               <Heading sx={boxHeading}>
                 $200
               </Heading>
               <Text sx={boxTextStyle}>
                 Covers all of a student&apos;s registration for the Fall Regional Conference.
               </Text>
-            </Box>
-            <Box sx={boxStyle}>
+            </Flex>
+            <Flex sx={boxStyle}>
               <Heading sx={boxHeading}>
                 $500
               </Heading>
               <Text sx={boxTextStyle}>
                 Covers all of a student&apos;s registration for the Fall Regional Conference.
               </Text>
-            </Box>
+            </Flex>
           </Flex>
           <Link href="https://securelb.imodules.com/s/1717/giving/interior.aspx?sid=1717&gid=2&pgid=403&cid=1031&dids=238&bledit=1" target="_blank" rel="noopener noreferrer">
-            <Button marginTop="3%" sx={donateButtonStyle}>
+            <Button marginTop="3%" sx={donateButtonStyle} w="100%">
               <Heading color='white'>
                 Donate Today!
               </Heading>
@@ -155,9 +176,8 @@ const Students = () => {
           </Link>
         </VStack>
       </Flex>
-      
     </main>
   );
 }
 
-export default Students;
+export default Alumni;
