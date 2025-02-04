d17f2e41913f4cbc7ee23420b1507885a78f0436
nsbecornell <91637107+nsbecornell@users.noreply.github.com>
2025-02-04 13:11:34 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   output: 'export',
   basePath: isProd ? '/nsbecornell.github.io' : '', // Add this line
-  assetPrefix: '/nsbecornell.github.io/', // Add this line
+  // assetPrefix: '/nsbecornell.github.io/', // Add this line
   images: {
     unoptimized: true, // Required for "output: export"
   },
