a7d4876305cfe3091ceceeb7ad64fd76364781a4
Chimdi <cornellnsbe@gmail.com>
2025-03-17 02:30:37 -0400

    Adjust size of text container to improve appearance on smaller screens

--- src/components/TextContainer.tsx
+++ src/components/TextContainer.tsx
@@ -21,9 +21,40 @@ const TextContainer = (props: {
   }
 
   return (
-    <HStack sx={boxStyling} direction='column' wrap='wrap' align='center' justify="center" w="fit-content" gap={5} padding={5}>
-      <Heading sx={textStyle} marginBottom={0} fontSize={25}>{props.title}</Heading>
-      <Text sx={textStyle} fontSize={20}>{props.message}</Text>
+    <HStack
+      sx={boxStyling}
+      direction='column'
+      wrap='wrap'
+      align='center'
+      justify="center"
+      w="fit-content"
+      gap={5}
+      padding={5}
+    >
+      <Heading
+        sx={textStyle}
+        marginBottom={0}
+        fontSize={
+          {
+            base: 25,
+            md: 25
+          }
+        }
+      >
+        {props.title}
+      </Heading>
+
+      <Text
+        sx={textStyle}
+        fontSize={
+          {
+            base: 20,
+            md: 20
+          }
+        }
+      >
+        {props.message}
+      </Text>
     </HStack>
   );
 }
